d4c8724494ce62f57f8c43668102362f004719c8
yusuff-fatorisa <bosun4real009@gmail.com>
2022-11-16 23:08:28 -0800

    fix Betty error in getLine.c

--- getLine.c
+++ getLine.c
@@ -110,7 +110,7 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 }
 
 /**
- * _getLine - gets the next line of input from STDIN
+ * _getline - gets the next line of input from STDIN
  * @info: parameter struct
  * @ptr: address of pointer to buffer, preallocated or NULL
  * @length: size of preallocated ptr buffer if not NULL
